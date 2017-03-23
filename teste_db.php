b41d6321ba18c10ff14ecc140a81dcc585991346
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2017-03-22 22:19:08 -0300

    Add files via upload

--- teste_db.php
+++ teste_db.php
@@ -7,20 +7,16 @@ $password = "SistemasInternet123";
 $dsn = "Driver={SQL Server};Server=$db_host;Port=1433;Database=$db_name;";
                
 if($db = odbc_connect(	$dsn, $user, $password)){
-	
-	if(odbc_exec($db, "INSERT INTO Usuario
-						(loginUsuario,
-						senhaUsuario,
-						nomeUsuario,
-						tipoPerfil,
-						usuarioAtivo) 
-					VALUES 
-						('luiz@gmail.com',
-						'123456',
-						'Luiz Fernando',
-						'A',
-						1)")){
-		echo "Usu&aacute;rio cadastrado com sucesso";					
+	echo "Iniciando Processamento<br>";
+	flush();
+	ob_flush();
+	sleep(5);
+	if(odbc_exec($db, " UPDATE Usuario
+						SET
+						loginUsuario = 'AN@gmail.com',
+						tipoPerfil = 'p' WHERE
+						loginUsuario = 'ANNA@gmail.com'")) {
+		echo "Usu&aacute;rio atualizado com sucesso<br>";					
 	}else{
 		echo "Erro ao cadastrar o usu&aacute;rio";
 	}
@@ -30,7 +26,36 @@ if($db = odbc_connect(	$dsn, $user, $password)){
 	echo ":-( N&atilde;o deu certo";
 	
 }
+//Deletar usuario
+echo "Agora vem o DELETE<br>";
+if(odbc_exec($db, " DELETE FROM Usuario
+						WHERE
+						loginUsuario = 'AN@gmail.com'")) {
+		echo "Usu&aacute;rio deletado com sucesso<br>";					
+	}else{
+		echo "Erro ao deletar o usu&aacute;rio";
+	}
+//-------------------------------------------------
+$query = odbc_exec($db, "SELECT * FROM Usuario");
+$result = odbc_fetch_array($query);
 
+echo "Id: {$result['idUsuario']} Nome: {$result['nomeUsuario']}<br>";
+while ($result = odbc_fetch_array($query)){
+	flush();
+	ob_flush();
+	sleep(1);
+	echo "Id: {$result['idUsuario']} Nome: {$result['nomeUsuario']}<br>";
+}
 
+//-------------------------------------------------
+$query = odbc_exec($db, "SELECT * FROM Usuario WHERE nomeUsuario LIKE 'A%'");
+$result = odbc_fetch_array($query);
 
+echo "Id: {$result['idUsuario']} Nome: {$result['nomeUsuario']}<br>";
+while ($result = odbc_fetch_array($query)){
+	flush();
+	ob_flush();
+	sleep(1);
+	echo "Id: {$result['idUsuario']} Nome: {$result['nomeUsuario']}<br>";
+}
 ?>
